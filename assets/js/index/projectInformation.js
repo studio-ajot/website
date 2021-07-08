11b1229d848b83973dcabe07971f8ad44e792417
ottovomdienst <77341550+ottovomdienst@users.noreply.github.com>
2021-07-08 10:44:30 +0200

    Update projectInformation.js

--- assets/js/index/projectInformation.js
+++ assets/js/index/projectInformation.js
@@ -2,12 +2,12 @@ const getProjectInformation = (indexOfImage) => {
   switch (indexOfImage) {
     case 0:
       return {
-        accentColor: "#3D4FD9",
+        accentColor: "#000080",
         projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
     case 1:
       return {
-        accentColor: "#F41649",
+        accentColor: "#000080",
         projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
     case 2:
@@ -17,34 +17,109 @@ const getProjectInformation = (indexOfImage) => {
       };
     case 3:
       return {
-        accentColor: "#FF0394",
+        accentColor: "#CDCD00",
         projectDescription: "Visitenkarten \nMarkenbecker / Northport",
       };
     case 4:
       return {
-        accentColor: "#FF0AF6",
+        accentColor: "#CD0000",
         projectDescription: "Calendarium Luminis \nJohann Sommer \nPlakat",
       };
     case 5:
       return {
-        accentColor: "#3BFF00",
+        accentColor: "#CD0000",
         projectDescription: "RESET \nVortragsreihe Muthesius Kunsthochschule \nPlakatgestaltung",
       };
     case 6:
       return {
-        accentColor: "#3D4FD9",
+        accentColor: "#CD0000",
         projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
     case 7:
       return {
-        accentColor: "#3D4FD9",
+        accentColor: "#ffc125",
         projectDescription: "Frauen*beratung in Elmshorn",
       };
     case 8:
       return {
-        accentColor: "#3D4FD9",
+        accentColor: "#ffc125",
         projectDescription: "Diversity Contexts in Teacher Education",
       };
+    case 9:
+      return {
+        accentColor: "#F41649",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };
+    case 10:
+      return {
+        accentColor: "#F41649",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };
+    case 11:
+      return {
+        accentColor: "#3A5FCD",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };
+    case 12:
+      return {
+        accentColor: "#FFEFD5",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };
+    case 13:
+      return {
+        accentColor: "#FFEFD5",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };
+    case 14:
+      return {
+        accentColor: "#006400",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };
+    case 15:
+      return {
+        accentColor: "#FF0394",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };
+    case 16:
+      return {
+        accentColor: "#FF4500",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };
+    case 17:
+      return {
+        accentColor: "#7FFFD4",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };
+    case 18:
+      return {
+        accentColor: "#7FFFD4",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };
+    case 19:
+      return {
+        accentColor: "#EEEE00",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };
+    case 20:
+      return {
+        accentColor: "#EEEE00",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };
+    case 21:
+      return {
+        accentColor: "#AFEEEE",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };
+    case 22:
+      return {
+        accentColor: "#F63B08",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };   
+     case 23:
+      return {
+        accentColor: "#00FF00",
+        projectDescription: "Diversity Contexts in Teacher Education",
+      };     
     default:
       return {
         accentColor: "black",
