50b395165289f78c7b7b60e733d09bc81c8caacc
ottovomdienst <77341550+ottovomdienst@users.noreply.github.com>
2021-07-08 10:53:27 +0200

    Update projectInformation.js

--- assets/js/index/projectInformation.js
+++ assets/js/index/projectInformation.js
@@ -18,107 +18,107 @@ const getProjectInformation = (indexOfImage) => {
     case 3:
       return {
         accentColor: "#CDCD00",
-        projectDescription: "Visitenkarten \nMarkenbecker / Northport",
+        projectDescription: "Visitenkarten \nMarkenbecker \nNorthport",
       };
     case 4:
       return {
         accentColor: "#CD0000",
-        projectDescription: "Calendarium Luminis \nJohann Sommer \nPlakat",
+        projectDescription: "Lüneburg mit anderen Augen \nFotograf Pablo Martínez-Calleja \nFotografische Publikation",
       };
     case 5:
       return {
         accentColor: "#CD0000",
-        projectDescription: "RESET \nVortragsreihe Muthesius Kunsthochschule \nPlakatgestaltung",
+        projectDescription: "Lüneburg mit anderen Augen \nFotograf Pablo Martínez-Calleja \nFotografische Publikation",
       };
     case 6:
       return {
         accentColor: "#CD0000",
-        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
+        projectDescription: "Lüneburg mit anderen Augen \nFotograf Pablo Martínez-Calleja \nFotografische Publikation",
       };
     case 7:
       return {
         accentColor: "#ffc125",
-        projectDescription: "Frauen*beratung in Elmshorn",
+        projectDescription: "Calendarium Luminis \nJohann Sommer \nPlakat",
       };
     case 8:
       return {
         accentColor: "#ffc125",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "Calendarium Luminis \nJohann Sommer \nPlakat",
       };
     case 9:
       return {
         accentColor: "#F41649",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "RESET \nVortragsreihe Muthesius Kunsthochschule \nPlakatgestaltung",
       };
     case 10:
       return {
         accentColor: "#F41649",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "RESET \nVortragsreihe Muthesius Kunsthochschule \nVorlagengestaltung",
       };
     case 11:
       return {
         accentColor: "#3A5FCD",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
     case 12:
       return {
         accentColor: "#FFEFD5",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
     case 13:
       return {
         accentColor: "#FFEFD5",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
     case 14:
       return {
         accentColor: "#006400",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
     case 15:
       return {
         accentColor: "#FF0394",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
     case 16:
       return {
         accentColor: "#FF4500",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
     case 17:
       return {
         accentColor: "#7FFFD4",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
     case 18:
       return {
         accentColor: "#7FFFD4",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
     case 19:
       return {
         accentColor: "#EEEE00",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
     case 20:
       return {
         accentColor: "#EEEE00",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
     case 21:
       return {
         accentColor: "#AFEEEE",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
     case 22:
       return {
         accentColor: "#F63B08",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };   
      case 23:
       return {
         accentColor: "#00FF00",
-        projectDescription: "Diversity Contexts in Teacher Education",
+        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };     
     default:
       return {
