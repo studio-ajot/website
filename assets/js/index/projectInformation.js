fd3fdba875dc5d5a66f43a236662314b1a130e63
Anastasia Marx <77341550+ottovomdienst@users.noreply.github.com>
2021-08-03 11:06:38 +0200

    Update projectInformation.js

--- assets/js/index/projectInformation.js
+++ assets/js/index/projectInformation.js
@@ -2,110 +2,125 @@ const getProjectInformation = (indexOfImage) => {
   switch (indexOfImage) {
     case 0:
       return {
-        accentColor: "#000080",
-        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
+        accentColor: "#02F4A9",
+        projectDescription: "Ultraviolett \nFlurin Jecker \nUmschlaggestaltung Haymon Verlag",
       };
     case 1:
+      return {
+        accentColor: "#02F4A9",
+        projectDescription: "Ultraviolett \nFlurin Jecker \nUmschlaggestaltung Haymon Verlag",
+      };
+    case 2:
+      return {
+        accentColor: "#02F4A9",
+        projectDescription: "Ultraviolett \nFlurin Jecker \nUmschlaggestaltung Haymon Verlag",
+      };
+    case 3:
       return {
         accentColor: "#000080",
         projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
-    case 2:
+    case 4:
+      return {
+        accentColor: "#000080",
+        projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
+      };
+    case 5:
       return {
         accentColor: "#FD009C",
         projectDescription: "Hallo! \nStudio Ajot \nPostkartengestaltung",
       };
-    case 3:
+    case 6:
       return {
         accentColor: "#CDCD00",
         projectDescription: "Northport \nMarkenbecker \nEntwicklung der Geschäftsausstattung",
       };
-    case 4:
-      return {
-        accentColor: "#CD0000",
-        projectDescription: "Lüneburg mit anderen Augen \nPablo Martínez-Calleja \nFotografische Publikation",
-      };
-    case 5:
-      return {
-        accentColor: "#CD0000",
-        projectDescription: "Lüneburg mit anderen Augen \nPablo Martínez-Calleja \nFotografische Publikation",
-      };
-    case 6:
-      return {
-        accentColor: "#CD0000",
-        projectDescription: "Lüneburg mit anderen Augen \nPablo Martínez-Calleja \nFotografische Publikation",
-      };
     case 7:
       return {
-        accentColor: "#ffc125",
-        projectDescription: "Calendarium Luminis \nJohann Sommer \nPlakatgestaltung \nKalendergestaltung",
+        accentColor: "#CD0000",
+        projectDescription: "Lüneburg mit anderen Augen \nPablo Martínez-Calleja \nFotografische Publikation",
       };
     case 8:
       return {
+        accentColor: "#CD0000",
+        projectDescription: "Lüneburg mit anderen Augen \nPablo Martínez-Calleja \nFotografische Publikation",
+      };
+    case 9:
+      return {
+        accentColor: "#CD0000",
+        projectDescription: "Lüneburg mit anderen Augen \nPablo Martínez-Calleja \nFotografische Publikation",
+      };
+    case 10:
+      return {
         accentColor: "#ffc125",
         projectDescription: "Calendarium Luminis \nJohann Sommer \nPlakatgestaltung \nKalendergestaltung",
       };
-    case 9:
+    case 11:
+      return {
+        accentColor: "#ffc125",
+        projectDescription: "Calendarium Luminis \nJohann Sommer \nPlakatgestaltung \nKalendergestaltung",
+      };
+    case 12:
       return {
         accentColor: "#F41649",
         projectDescription: "RESET \nVortragsreihe Muthesius Kunsthochschule \nPlakatgestaltung",
       };
-    case 10:
+    case 13:
       return {
         accentColor: "#F41649",
         projectDescription: "RESET \nVortragsreihe Muthesius Kunsthochschule \nGestaltung von Social Media Templates",
       };
-    case 11:
+    case 14:
       return {
         accentColor: "#3A5FCD",
         projectDescription: "DiCoT \nUniversität Hamburg \nLogoentwicklung",
       };
-    case 12:
+    case 15:
       return {
         accentColor: "#FFEFD5",
         projectDescription: "Parasite – Food Objects \nAnastasia Marx \nBuchgestaltung",
       };
-    case 13:
+    case 16:
       return {
         accentColor: "#FFEFD5",
         projectDescription: "Parasite – Food Objects \nAnastasia Marx \nBuchgestaltung",
       };
-    case 14:
+    case 17:
       return {
         accentColor: "#006400",
         projectDescription: "Div*erse Typ_en \nPlakatgestaltung",
       };
-    case 15:
+    case 18:
       return {
         accentColor: "#FF4500",
         projectDescription: "Frauen*beratung Elmshorn \nEntwicklung einer Corporate Identity ",
       };
-    case 16:
+    case 19:
       return {
         accentColor: "#FF4500",
         projectDescription: "Frauen*beratung Elmshorn \nGestaltung von Social Media Templates",
       };
-    case 17:
+    case 20:
       return {
         accentColor: "#7FFFD4",
         projectDescription: "Ultraviolett – Flurin Jecker \nHaymon Verlag \nBuchcovergestaltung",
       };
-    case 18:
+    case 21:
       return {
         accentColor: "#7FFFD4",
         projectDescription: "Ultraviolett – Flurin Jecker \nHaymon Verlag \nPlakatgestaltung",
       };
-    case 19:
+    case 22:
       return {
         accentColor: "#EEEE00",
         projectDescription: "Lose & Preserve – \nErinnerungen an Verlorenes \nJohanna Klüsener \nBuchgestaltung",
       };
-    case 20:
+    case 23:
       return {
         accentColor: "#EEEE00",
         projectDescription: "Lose & Preserve – \nErinnerungen an Verlorenes \nJohanna Klüsener \nBuchgestaltung",
       };
-    case 21:
+    case 24:
       return {
         accentColor: "#AFEEEE",
         projectDescription: "Testbild Zwei \nMascha Livanskaia \nKünstlerische Publikation",
@@ -115,7 +130,7 @@ const getProjectInformation = (indexOfImage) => {
         accentColor: "#F63B08",
         projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };   
-     case 23:
+     case 25:
       return {
         accentColor: "#00FF00",
         projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
