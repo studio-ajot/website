bd31ce9e8eedc392a1b5529f1c5b39763cd5ccc0
Anastasia Marx <77341550+ottovomdienst@users.noreply.github.com>
2021-11-18 17:41:34 +0100

    Update projectInformation.js

--- assets/js/index/projectInformation.js
+++ assets/js/index/projectInformation.js
@@ -18,12 +18,12 @@ const getProjectInformation = (indexOfImage) => {
      case 3:
       return {
         accentColor: "#FF00A7",
-        projectDescription: "MFA21.My Favourite Acronym \nMuthesius Kunsthochschule \nAusstellungsflyer",
+        projectDescription: "MFA21.My Favorite Acronym \nMuthesius Kunsthochschule \nAusstellungsflyer",
       };   
     case 4:
       return {
         accentColor: "#FF00A7",
-        projectDescription: "MFA21.My Favourite Acronym \nMuthesius Kunsthochschule \nAusstellungsflyer",
+        projectDescription: "MFA21.My Favorite Acronym \nMuthesius Kunsthochschule \nAusstellungsflyer",
       };        
     case 5:
       return {
