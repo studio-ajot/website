7c2e7eb0ad6614406825851f483d4f91cc9e3eb0
Anastasia Marx <77341550+ottovomdienst@users.noreply.github.com>
2022-06-08 17:00:06 +0200

    Update projectInformation.js

--- assets/js/index/projectInformation.js
+++ assets/js/index/projectInformation.js
@@ -9,7 +9,7 @@ const getProjectInformation = (indexOfImage) => {
       return {
         accentColor: "#000000",
         projectDescription: "getNextIT \nBranding \nLogogestaltung",
-      };         
+      };    
     case 2:
       return {
         accentColor: "#EB67B3",
@@ -18,129 +18,139 @@ const getProjectInformation = (indexOfImage) => {
     case 3:
       return {
         accentColor: "#EB67B3",
+        projectDescription: "MFA21.My Favorite Acronym \nMuthesius Kunsthochschule \nPostergestaltung",
+      };         
+    case 4:
+      return {
+        accentColor: "#EB67B3",
+        projectDescription: "MFA21.My Favorite Acronym \nMuthesius Kunsthochschule \nPostergestaltung",
+      };   
+    case 5:
+      return {
+        accentColor: "#EB67B3",
         projectDescription: "MFA21.My Favorite Acronym \nMuthesius Kunsthochschule \nAusstellungsflyer",
       };   
-    case 4:
+    case 6:
       return {
         accentColor: "#EB67B3",
         projectDescription: "MFA21.My Favorite Acronym \nMuthesius Kunsthochschule \nAusstellungsflyer",
       };  
-    case 5:
+    case 7:
       return {
         accentColor: "#EB67B3",
         projectDescription: "MFA21.My Favorite Acronym \nMuthesius Kunsthochschule \nAusstellungskatalog",
       };   
-     case 6:
+     case 8:
       return {
         accentColor: "#EB67B3",
         projectDescription: "MFA21.My Favorite Acronym \nMuthesius Kunsthochschule \nAusstellungskatalog",
       };      
-     case 7:
+     case 9:
       return {
         accentColor: "#D9C843",
         projectDescription: "DayOff \nBranding \nLogoentwicklung",
       };            
-    case 8:
+    case 10:
       return {
         accentColor: "#D3298C",
         projectDescription: "X-mas \nStudio Ajot \nPostkartengestaltung",
       };    
-    case 9:
+    case 11:
       return {
         accentColor: "#76EEC6",
         projectDescription: "Ultraviolett – Flurin Jecker \nHaymon Verlag \nCovergestaltung",
       };      
-    case 10:
+    case 12:
       return {
         accentColor: "#19C1A0",
         projectDescription: "Ultraviolett – Flurin Jecker \nHaymon Verlag \nPlakatgestaltung",
       };  
-    case 11:
+    case 13:
       return {
         accentColor: "#0C6E5B",
         projectDescription: "Naturheilpraxis Ulrike Röske \nBranding \nLogogestaltung",
       };         
-    case 12:
+    case 14:
       return {
         accentColor: "#1F1FD3",
         projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };
-    case 13:
+    case 15:
       return {
         accentColor: "#1F1FD3",
         projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
       };      
-    case 14:
+    case 16:
       return {
         accentColor: "#FD009C",
         projectDescription: "Hallo! \nStudio Ajot \nPostkartengestaltung",
       };
-    case 15:
+    case 17:
       return {
         accentColor: "#A29582",
         projectDescription: "Hoppe Gebäudetechnik \nMarkenbecker \nLogogestaltung",
       };
-    case 16:
-      return {
-        accentColor: "#E30000",
-        projectDescription: "Lüneburg mit anderen Augen \nPablo Martínez-Calleja \nFotografische Publikation",
-      };
-    case 17:
-      return {
-        accentColor: "#E30000",
-        projectDescription: "Lüneburg mit anderen Augen \nPablo Martínez-Calleja \nFotografische Publikation",
-      };
     case 18:
+      return {
+        accentColor: "#E30000",
+        projectDescription: "Lüneburg mit anderen Augen \nPablo Martínez-Calleja \nFotografische Publikation",
+      };
+    case 19:
+      return {
+        accentColor: "#E30000",
+        projectDescription: "Lüneburg mit anderen Augen \nPablo Martínez-Calleja \nFotografische Publikation",
+      };
+    case 20:
       return {
         accentColor: "#A0A0A0",
         projectDescription: "Studio Ajot \nVisitenkarten",
       };      
-    case 19:
+    case 21:
       return {
         accentColor: "#C3C3C3",
         projectDescription: "Calendarium Luminis \nJohann Sommer \nPlakatgestaltung \nKalendergestaltung",
       };
-    case 20:
+    case 22:
       return {
         accentColor: "#19C1A0",
         projectDescription: "RESET \nVortragsreihe Muthesius Kunsthochschule \nPlakatgestaltung",
       };
-    case 21:
+    case 23:
       return {
         accentColor: "#19C1A0",
         projectDescription: "RESET \nVortragsreihe Muthesius Kunsthochschule \nGestaltung von Social Media Templates",
       };
-    case 22:
+    case 24:
       return {
         accentColor: "#3A5FCD",
         projectDescription: "DiCoT \nUniversität Hamburg \nLogoentwicklung",
       };
-    case 23:
+    case 25:
       return {
         accentColor: "#000000",
         projectDescription: "Div*erse Typ_en \nPlakatgestaltung",
       };
-    case 24:
+    case 26:
       return {
         accentColor: "#0C6E5B",
         projectDescription: "Frauen*beratung Elmshorn \nProgrammheft 2022",
       };
-    case 25:
+    case 27:
       return {
         accentColor: "#0C6E5B",
         projectDescription: "Frauen*beratung Elmshorn \nInfo-Broschüre",
       };
-    case 26:
+    case 28:
       return {
         accentColor: "#0C6E5B",
         projectDescription: "Frauen*beratung Elmshorn \nEntwicklung einer Corporate Identity ",
       };
-    case 27:
+    case 29:
       return {
         accentColor: "#0C6E5B",
         projectDescription: "Frauen*beratung Elmshorn \nGestaltung von Social Media Templates",
       };
-    case 28:
+    case 30:
       return {
         accentColor: "#EEEE00",
         projectDescription: "Lose & Preserve – \nErinnerungen an Verlorenes \nJohanna Klüsener \nBuchgestaltung",
