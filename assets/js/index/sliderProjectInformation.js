322672b3595a8a0ba2b6aa05cd957db7f629e519
Anastasia Marx <77341550+ottovomdienst@users.noreply.github.com>
2022-11-08 17:53:37 +0100

    Update sliderProjectInformation.js

--- assets/js/index/sliderProjectInformation.js
+++ assets/js/index/sliderProjectInformation.js
@@ -1,104 +1,105 @@
 const projectInformation = [
   {
-    id: "project-21-epda-1",
+    id: "project-16-epda",
     accentColor: "#FF6343",
     projectDescription: "The design agency boost book \nepda \nBuchgestaltung",
     type: "img",
   },  
   {
-    id: "project-8-mfa-1",
+    id: "project-15-cb",
+    accentColor: "#0300E5",
+    projectDescription: "Ceramica Borealis \nKerstin Abraham \nBuchgestaltung",
+    type: "img",
+  }, 
+  {
+    id: "project-14-reset",
+    accentColor: "#19C1A0",
+    projectDescription:
+      "RESET \nVortragsreihe Muthesius Kunsthochschule \nPlakatgestaltung",
+    type: "img",
+  }, 
+  {
+    id: "project-14-openfba",
+    accentColor: "#2E3DFE",
+    projectDescription: "OpenFBA \nBranding \nLogoentwicklung",
+    type: "vid",
+  }, 
+  {
+    id: "project-12-mu",
+    accentColor: "#00CFDD",
+    projectDescription: "Gut zu wissen \nMuthesius Kunsthochschule \nEditorial Design",
+    type: "img",
+  }, 
+  {
+    id: "project-11-mfa",
     accentColor: "#EB67B3",
     projectDescription:
       "MFA21 – My Favorite Acronym \nMuthesius Kunsthochschule \nPostergestaltung",
     type: "img",
   },
   {
-    id: "project-8-mfa-3",
-    accentColor: "#EB67B3",
-    projectDescription:
-      "MFA21 – My Favorite Acronym \nMuthesius Kunsthochschule \nAusstellungskatalog",
+    id: "project-10-gNIT",
+    accentColor: "#000000",
+    projectDescription: "getNextIT \nBranding \nVisitenkarten",
     type: "img",
   },  
   {
-    id: "project-1-asepsis-2",
+    id: "project-9-x-mas",
+    accentColor: "#D3298C",
+    projectDescription: "X-mas \nStudio Ajot \nPostkartengestaltung",
+    type: "img",
+  },      
+  {
+    id: "project-8-ultraviolett",
+    accentColor: "#76EEC6",
+    projectDescription:
+      "Ultraviolett – Flurin Jecker \nHaymon Verlag \nCovergestaltung",
+    type: "img",
+  },    
+  {
+    id: "project-7-asepsis",
     accentColor: "#1F1FD3",
     projectDescription: "Asepsis \nChristian An \nKünstlerische Publikation",
     type: "img",
-  },
+  },   
   {
-    id: "project-2-openfba-1",
-    accentColor: "#2E3DFE",
-    projectDescription: "OpenFBA \nBranding \nLogoentwicklung",
-    type: "vid",
-  },
-  {
-    id: "project-3-reset-1",
-    accentColor: "#19C1A0",
-    projectDescription:
-      "RESET \nVortragsreihe Muthesius Kunsthochschule \nPlakatgestaltung",
+    id: "project-6-dayoff",
+    accentColor: "#D9C843",
+    projectDescription: "DayOff \nBranding \nLogoentwicklung",
     type: "img",
   },
   {
-    id: "project-4-fbe-1",
-    accentColor: "#0C6E5B",
-    projectDescription: "Frauen*beratung Elmshorn \nProgrammheft 2022",
+    id: "project-5-ajot-visitenkarte",
+    accentColor: "#A0A0A0",
+    projectDescription: "Studio Ajot \nVisitenkarten",
     type: "img",
-  },
+  },   
   {
-    id: "project-5-lmaa-2",
+    id: "project-4-diversity",
+    accentColor: "#000000",
+    projectDescription: "Div*erse Typ_en \nPlakatgestaltung",
+    type: "img",
+  },    
+  {
+    id: "project-3-lmaa",
     accentColor: "#E30000",
     projectDescription:
       "Lüneburg mit anderen Augen \nPablo Martínez-Calleja \nFotografische Publikation",
     type: "img",
   },
   {
-    id: "project-6-dayoff-1",
-    accentColor: "#D9C843",
-    projectDescription: "DayOff \nBranding \nLogoentwicklung",
-    type: "img",
-  },
-  {
-    id: "project-7-ultraviolett-1",
-    accentColor: "#76EEC6",
+    id: "project-2-calender",
+    accentColor: "#717171",
     projectDescription:
-      "Ultraviolett – Flurin Jecker \nHaymon Verlag \nCovergestaltung",
+      "Calendarium Luminis \nJohann Sommer \nPlakatgestaltung \nKalendergestaltung",
     type: "img",
   },
   {
-    id: "project-18-x-mas",
-    accentColor: "#D3298C",
-    projectDescription: "X-mas \nStudio Ajot \nPostkartengestaltung",
-    type: "img",
-  },  
-  {
-    id: "project-9-gNIT-1",
-    accentColor: "#000000",
-    projectDescription: "getNextIT \nBranding \nVisitenkarten",
-    type: "img",
-  },
-  {
-    id: "project-10-diversity",
-    accentColor: "#000000",
-    projectDescription: "Div*erse Typ_en \nPlakatgestaltung",
-    type: "img",
-  },
-  {
-    id: "project-10-epda-1",
-    accentColor: "#9A01A0",
-    projectDescription: "The Design Agency Boost Book \nepda \nBuchgestaltung",
-    type: "img",
-  },
-  {
-    id: "project-13-lap-detail",
+    id: "project-1-lap",
     accentColor: "#EEEE00",
     projectDescription:
       "Lose & Preserve – \nErinnerungen an Verlorenes \nJohanna Klüsener \nBuchgestaltung",
     type: "img",
   },
-  {
-    id: "project-20-ajot-visitenkarte",
-    accentColor: "#A0A0A0",
-    projectDescription: "Studio Ajot \nVisitenkarten",
-    type: "img",
-  },
+
 ];
