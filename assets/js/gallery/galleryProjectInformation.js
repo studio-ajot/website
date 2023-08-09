0b5f8a1ea24598b14ac3b23bce1fd589c1a14f97
Anastasia Marx <77341550+ottovomdienst@users.noreply.github.com>
2023-08-09 10:24:45 +0200

    Update galleryProjectInformation.js

--- assets/js/gallery/galleryProjectInformation.js
+++ assets/js/gallery/galleryProjectInformation.js
@@ -96,7 +96,7 @@ const galleryProjectInformation = [
   {
     id: "project-18-xmas23",
     accentColor: "#FC3642",
-    projectDescription: "POSTKARTE \nEine festliche Postkarte mit season's greetings für 2023 \n\n",
+    projectDescription: "POSTKARTE \nEine festliche Karte mit season's greetings für 2023 \n\n",
     type: "vid",
     categories: [""],
     mediaTypes: ["vid", "img"],
