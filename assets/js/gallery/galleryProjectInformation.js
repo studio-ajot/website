b4de2668e953f7993d76046a837413348ca54c9c
Anastasia Marx <77341550+ottovomdienst@users.noreply.github.com>
2024-03-12 11:23:38 +0100

    Update galleryProjectInformation.js

--- assets/js/gallery/galleryProjectInformation.js
+++ assets/js/gallery/galleryProjectInformation.js
@@ -7,8 +7,7 @@ const galleryProjectInformation = [
     categories: ["Editorial Design"],
     mediaTypes: ["img", "img", "img", "img", "img"],
     longTextDescription:
-      "Wolf Biermann: Visuelle Identität \n \nAuftraggeberIn: Clouds Hill, 2023-heute \nVisuelle Identität  \n \nWolf Biermann ist ein Künstler, der sich hat nie unterkriegen lassen und bis heute nicht müde wird, sich politisch zu engagieren. Der deutsche Lyriker und Liedermacher ist als politischer Rebell und scharfer Kritiker der DDR bekannt geworden. Bis heute hat er nicht an Aktualität verloren und soll sowohl bei langjährigen Biermann-Kenner:innen als auch bei einer neuen, jungen Zielgruppe sichtbar und zeitgemäß präsentiert werden. 
-
+      "Wolf Biermann: Visuelle Identität \n \nAuftraggeberIn: Clouds Hill, 2023-heute \nVisuelle Identität  \n \nWolf Biermann ist ein Künstler, der sich hat nie unterkriegen lassen und bis heute nicht müde wird, sich politisch zu engagieren. Der deutsche Lyriker und Liedermacher ist als politischer Rebell und scharfer Kritiker der DDR bekannt geworden. Bis heute hat er nicht an Aktualität verloren und soll sowohl bei langjährigen Biermann-Kenner:innen als auch bei einer neuen, jungen Zielgruppe sichtbar und zeitgemäß präsentiert werden. \n 
 Wir entwickelten ein neues, modernes System für sämtliche grafischen Assets, das Druckerzeugnisse wie Plattencover oder Flyer über Merch-Artikel wie Stifte oder Sticker bis hin zu Social-Media Templates und Spotify-Animationen abdeckt. Der charakteristische Bart und die Frisur schaffen als Kern-Visual im Logo klare Wiedererkennung. Variable Schritbreiten und der Einsatz von klaren, kontrastierenden Farben unterstützen visuell Biermanns Werk und Stil.,
   },
   {
