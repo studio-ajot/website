4f95d1c3a98a22364f60e5835042d42d2db1f5be
Anastasia Marx <77341550+ottovomdienst@users.noreply.github.com>
2024-04-30 17:11:11 +0200

    Update galleryProjectInformation.js

--- assets/js/gallery/galleryProjectInformation.js
+++ assets/js/gallery/galleryProjectInformation.js
@@ -25,7 +25,7 @@ const galleryProjectInformation = [
     projectDescription: "Wolf Biermann \nVisuelle Identität \n \nLogo-Entwicklung, Animation, Social Media, Werbemittel \nClouds Hill",
     type: "vid",
     categories: ["Branding"],
-    mediaTypes: ["vid", "img","img", "vid"],
+    mediaTypes: ["vid", "img","img", "img", "img","vid"],
     longTextDescription:
       "Wolf Biermann: Visuelle Identität \n \nAuftraggeberIn: Clouds Hill, 2023-heute \nVisuelle Identität  \n \nDer deutsche Lyriker und Liedermacher Wolf Biermannn ist als politischer Rebell und scharfer Kritiker der DDR bekannt geworden. Bis heute hat er nicht an Aktualität verloren und soll sowohl bei langjährigen Biermann-Kenner:innen als auch bei einer neuen, jungen Zielgruppe sichtbar und zeitgemäß präsentiert werden. \nWir entwickelten ein neues, modernes System für sämtliche grafischen Assets, das Druckerzeugnisse wie Plattencover oder Flyer über Merch-Artikel wie Stifte oder Sticker bis hin zu Social-Media Templates und Spotify-Animationen abdeckt.",
   }, 
