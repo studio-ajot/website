f3a2ee99dd0e47eee1bfc2082712aae537f3e8eb
Anastasia Marx <77341550+ottovomdienst@users.noreply.github.com>
2024-10-16 15:37:12 +0200

    Update galleryProjectInformation.js

--- assets/js/gallery/galleryProjectInformation.js
+++ assets/js/gallery/galleryProjectInformation.js
@@ -44,7 +44,7 @@ const galleryProjectInformation = [
     accentColor: "#A022C3",
     projectDescription: "BUCHGESTALTUNG \nJubiläums-Buch \n \nDesign Agency Boost Book, \nEuropean Brand and Packaging Design Association",
     type: "img",
-    categories: ["Buchgestaltung & Editorial Design"],
+    categories: ["Buchgestaltung", "Editorial Design"],
     mediaTypes: ["img", "img", "img", "img", "vid", "img", "img", "img", "img", "img", "img", "img", "img", "img", "img", "img", "img", "img"],
     longTextDescription:
       "The Design Agency Boost Book \n \nAuftraggeberIn: European Brand & Packaging Design Association, 2022 \nBuchgestaltung  \n \nDie european brand & packaging design association ist das führende europäische Netzwerk internationaler GestalterInnen und Agenturen. Im Fokus stehen der Austausch von Informationen, Ideen und Meinungen über Verpackung, Identitäten oder Literaturgeschehen. \nAnlässlich des 30-jährigen Jubiläums gestalteten wir „The Design Agency Boost Book“ - ein Buch, das die zehn wichtigsten Überzeugungen von Fachleuten aus der internationalen Gestaltungsbranche vorstellt.",
@@ -84,7 +84,7 @@ const galleryProjectInformation = [
     accentColor: "#02204A",
     projectDescription: "CORPORATE DESIGN \nWebseite\n\n",
     type: "img",
-    categories: ["Corporate Design, Webdesign"],
+    categories: ["Branding", "Webdesign"],
     mediaTypes: ["img", "vid"],
     longTextDescription:
       "B310 digital  \nAuftraggeber:in: B310 digital, 2024  \nCorporate Identity, Webseite  \n \nErstellung einer Corporate Identity.",
@@ -94,7 +94,7 @@ const galleryProjectInformation = [
     accentColor: "#A20E6D",
     projectDescription: "POSTKARTE \nNeujahrsgrüße, Schriftgestaltung \n\n",
     type: "vid",
-    categories: [""],
+    categories: ["Editorial Design"],
     mediaTypes: ["vid", "img"],
     longTextDescription:
       "Ein Neujahrsgruß für 2022: be inspired, happy, healthy, fancy, playful, loving, fierce, mindful, focused, bold, sharp, curious",
@@ -104,7 +104,7 @@ const galleryProjectInformation = [
     accentColor: "#454342",
     projectDescription: "BUCHGESTALTUNG \nKünstlerinnen-Publikation \n \nCeramica Borealis 07 – Disegno Ornamento, \nKerstin Abraham",
     type: "img",
-    categories: ["Buchgestaltung & Editorial Design"],
+    categories: ["Buchgestaltung", "Editorial Design"],
     mediaTypes: ["img", "img", "img", "img", "img", "img", "img", "img", "img", "img", "img", "img", "img", "img", "img"],
     longTextDescription:
       "Ceramica Borealis 07 – Disegno Ornamento \n  \nAuftraggeberIn: Kerstin Abraham, 2022 \nKünstlerinnen-Publikation \n  \n»Ich sehe ein Nebeneinander in Nachbarschaften, in Verwandtschaften und formalen Wiederholungen. Ich sehe ein Übereinander, ein Untereinander; ein Voreinander und ein Hintereinander in Schichten. Ein (Übereinander-)»Geschichte«. Geschichte. Eine Geschichte?«¹ \nMit der Reihe CERAMICA BOREALIS will die Sparkassenstiftung Schleswig-Holstein der zeitgenössischen Künstlerschaft des Nordens ein Forum verschaffen, die sich im Bereich der künstlerischen Keramik bewegt. Für die 7. Ausgabe dieser Reihe wurden wir mit der Gestaltung beauftragt. Gezeigt werden die Arbeit von Kerstin Abraham, die Installationsreihe »Disegno Ornamento«, sowie die einzelnen keramischen und nicht-keramischen Bestandteile der Arbeiten.\n¹ Zitat: Lena Kaapke",
@@ -126,7 +126,7 @@ const galleryProjectInformation = [
     projectDescription:
       "AUSSTELLUNGSDESIGN \nVisuelle Identität, Plakatgestaltung, Webdesign \n\nMFA21 – My Favorite Acronym, \nMuthesius Kunsthochschule, Kiel",
     type: "img",
-    categories: ["Branding", "Buchgestaltung & Editorial Design", "Digital"],
+    categories: ["Branding", "Buchgestaltung", "Editorial Design", "Digital"],
     mediaTypes: ["img", "img", "img", "vid", "img", "img", "img", "img", "img", "img", "img", "img", "img", "img"],
     longTextDescription:
       "MFA - My Favorite Acronym \n \nAuftraggeberIn: Muthesius Kunsthochschule, 2021 \nVisuelle Identität mit Plakat, Flyer, Katalog, Webdesign  \nKonzept: Lena Reisner \nProgrammierung: Björn Schmidt \n \nFür die Ausstellung »Master of Fine Arts« der MasterabsolventInnen der Muthesius Kunsthochschule, Kiel im Bereich Freie Kunst entwickelten wir 2021 die visuelle Erscheinung. Die Gestaltung basiert auf dem Konzept, dass das Akronym des akademischen Titels MFA (Master of Fine Arts) individuell für eine Reihe anderer, sowohl real existierender als auch fiktionaler Ausdrücke und Eigennamen stehen kann.",
@@ -137,7 +137,7 @@ const galleryProjectInformation = [
     projectDescription:
       "VISUELLE IDENTITÄT  \nPlakatgestaltungn \n\nReset – Let’s talk about diversity, \nVortragsreihe der Muthesius Kunsthochschule, Kiel",
     type: "img",
-    categories: ["Branding", "Buchgestaltung & Editorial Design"],
+    categories: ["Branding", "Buchgestaltung", "Editorial Design"],
     mediaTypes: ["img", "img", "img", "vid", "img"],
     longTextDescription:
       "RESET – let’s talk about diversity  \n \nAuftraggeberIn: Muthesius Kunsthochschule, 2020 \nPlakatreihe, Social Media Vorlagen  \n \nChancengleichheit, Antidiskriminierung und Diversität sind prozesshafte Themenkomplexe, die es stetig zu diskutieren gilt. Insbesondere an Institutionen ist ein gemeinsamer Austausch essenziell. Für die Themen- und Diskursveranstaltungen der Arbeitsgruppe RESET, bestehend aus unterschiedlichen AkteurInnen der Hochschule, gestalteten wir Plakate und Social Media Vorlagen.",
@@ -167,7 +167,7 @@ const galleryProjectInformation = [
     accentColor: "#12847E",
     projectDescription: "VISUELLE MARKENIDENTITÄT \nLogoentwicklung, Geschäftsausstattung \n\nFrauen*beratung Elmshorn",
     type: "img",
-    categories: ["Branding", "Buchgestaltung & Editorial Design", "Digital"],
+    categories: ["Branding", "Buchgestaltung", "Editorial Design", "Digital"],
     mediaTypes: ["img", "img", "img", "img", "img", "img", "vid", "img", "img"],
     longTextDescription:
       "Frauen*beratung Elmshorn \n \nAuftraggeberIn: Frauen*beratung Elmshorn, 2021 - heute \nIdentität mit Logo, Template, Flyer, Jahresbericht \n \nDie Frauen*beratung Elmshorn ist ein Treffpunkt und eine Beratungsstelle von Frauen für Frauen. \n2020 entwickelten wir die neue visuelle Identität und gestalten seidem fortlaufend Programmhefte, Flyer, Jahresberichte und Social Media Vorlagen.",
@@ -188,7 +188,7 @@ const galleryProjectInformation = [
     accentColor: "#FC3642",
     projectDescription: "POSTKARTE \n\nseason's greetings",
     type: "vid",
-    categories: [""],
+    categories: ["Editorial Design"],
     mediaTypes: ["vid", "img"],
     longTextDescription:
       "Eine festliche Karte mit season's greetings für 2023",
@@ -199,7 +199,7 @@ const galleryProjectInformation = [
     projectDescription:
       "BUCHGESTALTUNG \nCovergestaltung \n\nUltraviolett von Flurin Jecker \nHaymon Verlag",
     type: "img",
-    categories: ["Buchgestaltung & Editorial Design"],
+    categories: ["Buchgestaltung", "Editorial Design"],
     mediaTypes: ["img", "img", "img", "img", "img"],
     longTextDescription:
       "Ultraviolett \n \nAuftraggeberIn: Haymon Verlag, 2021 \nUmschlaggestaltung \n \n»Es gibt einen Punkt, an dem Hirngespinste zu Geisterbeschwörungen werden. Und es gibt einen Punkt, an dem sich Panik in Musik verwandelt. Durch eine bestimmte Feinsinnigkeit lassen sich diese beiden Punkte überlagern. Dort beginnt es zu glühen, dort gibt es Sonnenuntergänge, Abschiede und Euphorie. Von diesem Glühen erzählt Flurin Jecker.« ¹  in Ultraviolett. Für die Umschlaggestaltung des Romans wurden wir vom Haymon Verlag beauftragt. \n¹ Zitat: Joshua Groß", 
@@ -220,7 +220,7 @@ const galleryProjectInformation = [
     projectDescription:
       "BUCHGESTALTUNG \nFotografische Publikation \n\nLüneburg mit anderen Augen, \nPablo Martínez-Calleja",
     type: "img",
-    categories: ["Buchgestaltung & Editorial Design"],
+    categories: ["Buchgestaltung", "Editorial Design"],
     mediaTypes: ["img", "img", "img", "img", "img", "img", "img", "img", "img", "img", "img", "img", "img"],
     longTextDescription:
       "Lüneburg mit anderen Augen \n \nAuftraggeberIn: Pablo Martínez-Calleja, 2021 \nMit Fotografien von Pablo Martínez-Calleja \nBuchgestaltung \n \nWie ein Jäger des Augenblicks streift der in Lüneburg lebende Fotograf Pablo Martínez-Calleja mit seiner Kamera umher und nähert sich der Realität der StadtbewohnerInnen in Zeiten der Pandemie. Er fängt Situationen mit einem sensiblen Blick und ironischem Augenzwinkern ein. Jedes Foto lässt die Betrachtenden die Präsenz des Autors spüren, die wir auch innerhalb der Buchgestaltung in den Fokus setzten. Wir bewegen uns mit den Augen und der Perspektive des Fotografen innerhalb des Buches durch die Stadt.",
@@ -231,7 +231,7 @@ const galleryProjectInformation = [
     projectDescription:
       "PLAKATGESTALTUNG \nKalendergestaltung \n\nCalendarium Luminis, \nJohann Sommer",
     type: "img",
-    categories: ["Branding", "Buchgestaltung & Editorial Design", "Infografik"],
+    categories: ["Branding", "Buchgestaltung", "Editorial Design", "Infografik"],
     mediaTypes: ["img", "img", "img", "img", "img"],
     longTextDescription:
       "Calendarium Luminis \n \nAuftraggeberIn: Johann Sommer, 2019 \nPlakatgestaltung, Kalendergestaltung  \n300 signierte Exemplare \neinseitiger Sonderfarbendruck in Silber auf schwarzem und dunkelblauen Karton.  \n \nFür den Kieler Künstler Johann Sommer gestalteten wir 2021 den Licht-Kalender im Posterformat. Der Calendarium Luminis zeigt die Monate und Wochentage, die Tierkreiszeichen des Mondes, Mondphasen, Stundenringe mit Sonnen- und Mondscheindauer, Tierkreiszeichen der Sonne und die Sonnenfeste. Zusätzlich lassen sich die Auf- und Untergangszeiten von Sonne und Mond für jeden Tag des Kalenderjahres 2021 für den Standort Kiel ablesen.",
@@ -241,7 +241,7 @@ const galleryProjectInformation = [
     accentColor: "#0738A5",
     projectDescription: "BUCHGESTALTUNG \nTextsatz \n\nAuswahl",
     type: "img",
-    categories: ["Buchgestaltung & Editorial Design"],
+    categories: ["Buchgestaltung", "Editorial Design"],
     mediaTypes: ["img", "img", "img", "img"],
     longTextDescription:
       "Buchsatz \n \nAuftraggeberIn: unterschiedliche AuftraggeberInnen \nJahre: 2020 bis heute \n \n",
@@ -261,7 +261,7 @@ const galleryProjectInformation = [
     accentColor: "#00CFDD",
     projectDescription: "BUCHGESTALTUNG \nEditorial Design \n\nGut zu wissen – Alles für den Studienbeginn, \nMuthesius Kunsthochschule, Kiel",
     type: "img",
-    categories: ["Buchgestaltung & Editorial Design"],
+    categories: ["Buchgestaltung", "Editorial Design"],
     mediaTypes: ["img", "img", "img", "img", "img", "img"],
     longTextDescription:
       "Gut zu wissen – Alles für den Studienbeginn \n \nAuftraggeberIn: Muthesius Kunsthochschule, 2023 \nEditorial Design  \nInteraktives PDF \n \nDas erste Semester ist für viele Studierende eine besondere und aufregende Zeit. Es gibt viele Fragen und eine Menge zu bedenken. Für einen möglichst entspannten und stressfreien Einstieg ins Studium wurde dieser Leitfaden erstellt, für dessen Gestaltung wir beauftragt wurden. Er soll einen Überblick über relevante Informationen zum Studienbeginn geben und dabei helfen, die wichtigsten Fragen zu beantworten.",
@@ -271,7 +271,7 @@ const galleryProjectInformation = [
     accentColor: "#1F1FD3",
     projectDescription: "BUCHGESTALTUNG \nKünstlerische Publikation \n\nAsepsis, \nChristian An",
     type: "img",
-    categories: ["Buchgestaltung & Editorial Design"],
+    categories: ["Buchgestaltung", "Editorial Design"],
     mediaTypes: ["img", "img", "img", "img", "img", "img"],
     longTextDescription:
       "Asepsis \n \nAuftraggeberIn: Christian An, 2019 \nKünstlerpublikation \n300 signierte Exemplare  \n \nAuf 19 Seiten inszenierten wir Zeichnungen des Künstlers Christian An und gestalteten das Künstlerbuch Asepsis. An zeigt von Reinlichkeit besessene Akteure – Gurus mit riesigen Zahnbürsten. Gelb-blaue Müllmänner, Verbotsschilder und Absperrbänder verweisen in ihrer signalhaften Farbigkeit auf Europol, die europäische Polizei. Was auf den ersten Blick noch humorvoll erscheint, beschäftigt sich kritisch damit, was – von Hierarchiehörigkeit bis hin zu Stigmatisierung – mit einer Gesellschaft geschehen kann, wenn sich Hygiene zur Ideologie und Keimfreiheit zum Heilsversprechen wandelt.",
