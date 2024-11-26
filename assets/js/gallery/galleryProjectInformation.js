bf1445823c686e8cd42f3765ae825106c07ba2cb
Anastasia Marx <77341550+ottovomdienst@users.noreply.github.com>
2024-11-26 17:06:07 +0100

    Update galleryProjectInformation.js

--- assets/js/gallery/galleryProjectInformation.js
+++ assets/js/gallery/galleryProjectInformation.js
@@ -97,7 +97,7 @@ const galleryProjectInformation = [
     categories: ["Illustration", "Buch- & Editorial Design"],
     mediaTypes: ["img"],
     longTextDescription:
-      "CORPORATE DESIGN, PLAKATGESTALTUNG, ANIMATION, DIGITAL DESIGN \n \nAnimation & Social Media Kampagnen für Wolf Biermann, 2023-heute \n \nDer deutsche Lyriker und Liedermacher Wolf Biermannn ist als politischer Rebell und scharfer Kritiker der DDR bekannt geworden. Bis heute hat er nicht an Aktualität verloren und soll sowohl bei langjährigen Biermann-Kenner:innen als auch bei einer neuen, jungen Zielgruppe sichtbar und zeitgemäß präsentiert werden. \nWir entwickelten ein neues, modernes System für sämtliche grafischen Assets, das Druckerzeugnisse wie Plattencover oder Flyer über Merch-Artikel wie Stifte oder Sticker bis hin zu Social-Media Templates und Spotify-Animationen abdeckt.",
+      "BUCH- UND EDITORIAL DESIGN, ILLUSTRATION \n \nEditorial Design, Art Direction und Illustrationen für ein Reisetagebuch",
   }, 
   {
     id: "project-17-xmas22",
