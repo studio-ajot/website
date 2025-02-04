95e008d116e12c89197d60eca66f14f91118d476
Anastasia Marx <77341550+ottovomdienst@users.noreply.github.com>
2025-02-04 16:53:40 +0100

    Update galleryProjectInformation.js

--- assets/js/gallery/galleryProjectInformation.js
+++ assets/js/gallery/galleryProjectInformation.js
@@ -40,7 +40,7 @@ const galleryProjectInformation = [
       "CORPORATE DESIGN, MUSIK  \n \nAufbau einer Markenidentität & Plattengestaltung für Wolf Biermann, 2023-heute \n \nDer deutsche Lyriker und Liedermacher Wolf Biermannn ist als politischer Rebell und scharfer Kritiker der DDR bekannt geworden. Bis heute hat er nicht an Aktualität verloren und soll sowohl bei langjährigen Biermann-Kenner:innen als auch bei einer neuen, jungen Zielgruppe sichtbar und zeitgemäß präsentiert werden. \nWir entwickelten ein neues, modernes System für sämtliche grafischen Assets, das Druckerzeugnisse wie Plattencover oder Flyer über Merch-Artikel wie Stifte oder Sticker bis hin zu Social-Media Templates und Spotify-Animationen abdeckt. \nFotos von Wolf Biermann: Jonas Albrecht | www.jonas-albrecht.de | @zwischensequenzen",
   },
   {
-    id: "project-29–b310",
+    id: "project-29-b310",
     accentColor: "#FF0055",
     projectDescription: "CORPORATE DESIGN, WEBDESIGN  \n \nWebdesign für B310 digital",
     type: "img",
