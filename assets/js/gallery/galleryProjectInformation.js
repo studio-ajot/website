055b6608b61009da0c77c2e4f8538d20894157ae
Anastasia Marx <77341550+ottovomdienst@users.noreply.github.com>
2025-02-04 11:41:52 +0100

    Update galleryProjectInformation.js

--- assets/js/gallery/galleryProjectInformation.js
+++ assets/js/gallery/galleryProjectInformation.js
@@ -5,7 +5,7 @@ const galleryProjectInformation = [
     projectDescription: "BRANDING \n \nCorporate Design für colibris",
     type: "img",
     categories: ["Branding", "Corporate Design"],
-    mediaTypes: ["img", "vid", "img", "img", "img", "img"],
+    mediaTypes: ["img", "vid", "img", "img", "img", "img",  "img"],
     longTextDescription:
       "BRANDING \n \nCorporate Design für colibris \n \nZum 25-jährigen Jubiläum entwickelten wir für Colibris Eyewear ein modernes Corporate Design, das die Markenidentität neu definiert. Unter dem Leitgedanken »Great glasses for small faces« verbindet das visuelle Konzept nordisches Design mit einer zeitgemäßen Ästhetik. Ein maritimes Blau und warmer Sandton spiegeln die norddeutsche Landschaft wider, während das ikonische »C« und filigrane Wellenmuster die Herkunft und Präzision der Marke unterstreichen. Der überarbeitete Logo-Schriftzug kombiniert Eleganz mit Selbstbewusstsein und setzt die Spezialisierung auf Brillen für schmale Gesichter eindrucksvoll in Szene. \nMessefotos ©reizueberflutung.com ",
   },    
