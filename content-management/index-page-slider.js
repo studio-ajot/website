13ce3926a70271b7ac87a27158f75e160f2fc775
Ralf Duong <r.duong@live.de>
2025-07-31 16:33:41 +0200

    Rename main statements carousel to customer statements carousel and update related styles

--- content-management/index-page-slider.js
+++ content-management/index-page-slider.js
@@ -18,7 +18,7 @@ const indexPageSlider = [
 ];
 
 
-const indexPageStatementsSlider = [
+const customerStatementsSlider = [
     {
         author: "Sabine Wojcieszak",
         subtitle: "getNextIT",
