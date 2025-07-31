1bf594b8000d73ddcc2997dceafb92d73b76a101
Anastasia Marx <77341550+ottovomdienst@users.noreply.github.com>
2025-07-31 16:38:01 +0200

    Update index-page-slider.js

--- content-management/index-page-slider.js
+++ content-management/index-page-slider.js
@@ -21,12 +21,12 @@ const indexPageSlider = [
 const indexPageStatementsSlider = [
     {
         author: "Sabine Wojcieszak",
-        subtitle: "getNextIT",
+        subtitle: "Gründerin von getNextIT",
         text: "»… Johanna und Anastasia haben gute Fragen gestellt, extrem gut zugehört und meine Gedanken sowie mein Feedback aufgegriffen und umgesetzt. Sich in diesem Prozess gehört zu fühlen, ist meines Erachtens wichtig dafür, um sich später mit dem Ergebnis identifizieren zu können. …«",
         link: 'https://www.linkedin.com/pulse/zeit-f%25C3%25BCr-ein-neues-gesicht-mehr-als-nur-logo-getnext-it/?trackingId=zFekC7I%2FgDhA5bWNS334%2FA%3D%3D"',
     }, {
         author: "Corin Freyer",
-        subtitle: "DayOff",
+        subtitle: "Gründer von DayOff",
         text: "»Die Zusammenarbeit mit Studio Ajot war ein Traum. Ein sehr professionelles Team, welches mit uns gemeinsam und Schritt für Schritt ein neues Unternehmensdesign entworfen hat. Sehr zu empfehlen!«",
     }, {
         author: "Pablo Martínez-Calleja",
