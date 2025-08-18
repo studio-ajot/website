6ac98bc6d128e4ea671f3c91c34d7b0c82f567a7
Anastasia Marx <anastasia.marx@yahoo.de>
2025-08-18 16:14:25 +0200

    Update project-information.js

--- content-management/project-information.js
+++ content-management/project-information.js
@@ -24,11 +24,11 @@ const projectInformation = [
     {
         id: "plakatdesign-musik-rising-stars",
         accentColor: "#9DB7FF",
-        title: "Great glasses for small faces",
+        title: "Rising Stars 2025",
         categories: ["Corporate Design", "Musik", "Plakatgestaltung", "Animation"],
         metaData: "Gestaltung für Rising Stars",
         mediaTypes: ["img", "img"],
-        description: "Zum 25-jährigen Jubiläum entwickelten wir für Colibris Eyewear ein modernes Corporate Design, das die Markenidentität neu definiert. Unter dem Leitgedanken »Great glasses for small faces« verbindet das visuelle Konzept nordisches Design mit einer zeitgemäßen Ästhetik. Ein maritimes Blau und warmer Sandton spiegeln die norddeutsche Landschaft wider, während das ikonische »C« und filigrane Wellenmuster die Herkunft und Präzision der Marke unterstreichen. Der überarbeitete Logo-Schriftzug kombiniert Eleganz mit Selbstbewusstsein und setzt die Spezialisierung auf Brillen für schmale Gesichter eindrucksvoll in Szene. <br>Messefotos ©reizueberflutung.com"
+        description: ""
     },
     {
         id: "lp-design-wolf-biermann-box",
