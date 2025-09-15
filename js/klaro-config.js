c835a2fbbb367472fb231b3db60edc66d421e419
Ralf Duong <r.duong@live.de>
2025-09-15 14:06:00 +0200

    Update config for clarity

--- js/klaro-config.js
+++ js/klaro-config.js
@@ -34,7 +34,7 @@ var klaroConfig = {
                         t.src = "https://www.clarity.ms/tag/" + i;
                         y = l.getElementsByTagName(r)[0];
                         y.parentNode.insertBefore(t, y);
-                    })(window, document, "clarity", "script", "t4i0065bxt");
+                    })(window, document, "clarity", "script", "t2e4uqcpzi");
                 }
             }
         }
