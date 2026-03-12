8bcfd637c92f33b7b2bf06b89f2c8f5844a450da
Anastasia Marx <anastasia.marx@yahoo.de>
2026-03-12 15:10:40 +0100

    Update project-information.js

--- content-management/project-information.js
+++ content-management/project-information.js
@@ -15,11 +15,11 @@ const projectInformation = [
  {
         id: "webdesign-fotografie-elenov",
         accentColor: "#9DB7FF",
-        title: "Great glasses for small faces",
-        categories: ["Corporate Design", "Packaging"],
-        metaData: "Corporate Design für colibris",
-        mediaTypes: ["img", "vid", "img", "img", "img", "vid", "img", "img", "img", "img", "img"],
-        description: "Zum 25-jährigen Jubiläum entwickelten wir für Colibris Eyewear ein modernes Corporate Design, das die Markenidentität neu definiert. Unter dem Leitgedanken »Great glasses for small faces« verbindet das visuelle Konzept nordisches Design mit einer zeitgemäßen Ästhetik. Ein maritimes Blau und warmer Sandton spiegeln die norddeutsche Landschaft wider, während das ikonische »C« und filigrane Wellenmuster die Herkunft und Präzision der Marke unterstreichen. Der überarbeitete Logo-Schriftzug kombiniert Eleganz mit Selbstbewusstsein und setzt die Spezialisierung auf Brillen für schmale Gesichter eindrucksvoll in Szene. <br>Messefotos ©reizueberflutung.com"
+        title: "Webdesign: Elenov Photography",
+        categories: ["Webdesign"],
+        metaData: "Webdesign",
+        mediaTypes: ["img", "img", "img", "img"],
+        description: "Webdesign"
     },
     {
         id: "corporate-design-colibris-eyewear",
