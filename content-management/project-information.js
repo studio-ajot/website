688641a3d98b6ee3de77aca9ce519d7e38d1c44d
Anastasia Marx <77341550+ottovomdienst@users.noreply.github.com>
2026-03-30 14:46:43 +0200

    Update project-information.js

--- content-management/project-information.js
+++ content-management/project-information.js
@@ -49,7 +49,7 @@ const projectInformation = [
         description: "Zum 25-jährigen Jubiläum entwickelten wir für Colibris Eyewear ein modernes Corporate Design, das die Markenidentität neu definiert. Unter dem Leitgedanken »Great glasses for small faces« verbindet das visuelle Konzept nordisches Design mit einer zeitgemäßen Ästhetik. Ein maritimes Blau und warmer Sandton spiegeln die norddeutsche Landschaft wider, während das ikonische »C« und filigrane Wellenmuster die Herkunft und Präzision der Marke unterstreichen. Der überarbeitete Logo-Schriftzug kombiniert Eleganz mit Selbstbewusstsein und setzt die Spezialisierung auf Brillen für schmale Gesichter eindrucksvoll in Szene. <br>Messefotos ©reizueberflutung.com"
     },
     {
-        id: "corporate-design-architekrur-relab",
+        id: "corporate-design-architektur-relab",
         accentColor: "#FC4F5F",
         title: "Reallabore zu nachhaltigen Bauweisen",
         categories: ["Corporate Design"],
